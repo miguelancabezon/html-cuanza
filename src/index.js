fc8e4e692e01887f2d60b34f89e92001184fd1db
miguel.cabezon <miguelangel.cabezon@uneatlantico.es>
2024-06-19 17:06:17 +0200

    Changes to be committed:

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 console.log("LANG " + document.documentElement.lang);
+
+/* Listeners */
+
+document.getElementById("top-toggler").addEventListener("click", (el) => {
+  let topMenu = document.getElementsByClassName("top-navigation")[0];
+  let toggler = el.target;
+
+  if (topMenu === null || topMenu === undefined) {
+    return;
+  }
+
+  if (toggler === null || toggler === undefined) {
+    return;
+  }
+
+  if (topMenu.style.display === "flex") {
+    topMenu.style.display = "none";
+    toggler.style.transform = "rotate(0deg)";
+  } else {
+    topMenu.style.display = "flex";
+    toggler.style.transform = "rotate(90deg)";
+  }
+});
+
+/* Functions */
+
+/* ----  To Top button: Hide on Top, Show when Scroll --- */
+// Get the button:
+let mybutton = document.getElementById("to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
